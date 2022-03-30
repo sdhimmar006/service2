87e7a538e9c5a1ef45c775ece1f82bdc87bbfb1e
Swapnil Dhimmar <sdhimmar006@gmail.com>
2022-03-30 16:43:20 +0530

    use gcp lib

--- src/main/java/com/example/demo/WebController.java
+++ src/main/java/com/example/demo/WebController.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,6 +15,8 @@ public class WebController {
 
     @Value("${sleep}")
     Integer sleep;
+
+    private static final Logger logger = LoggerFactory.getLogger(WebController.class);
 
     @GetMapping("/service2")
     @SneakyThrows
@@ -27,6 +31,7 @@ public class WebController {
     @SneakyThrows
     public ResponseEntity<String> errorEndpoint(){
         log.error("some random error");
+        logger.error("Logging ERROR with Logback");
         return ResponseEntity.ok("From service2 error");
     }
 }
