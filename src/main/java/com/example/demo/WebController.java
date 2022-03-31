795faa5387486febac9a58ddd4d5e5eb4ec93e05
Swapnil Dhimmar <sdhimmar006@gmail.com>
2022-03-31 12:20:13 +0530

    use slf4j

--- src/main/java/com/example/demo/WebController.java
+++ src/main/java/com/example/demo/WebController.java
@@ -10,19 +10,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller("/")
-//@Slf4j
+@Slf4j
 public class WebController {
 
     @Value("${sleep}")
     Integer sleep;
-    private static final Logger logger = LoggerFactory.getLogger(WebController.class);
-
 //    private static final Logger logger = LoggerFactory.getLogger(WebController.class);
 
     @GetMapping("/service2")
     @SneakyThrows
     public ResponseEntity<String> postMsg(){
-//        log.info("came to service2");
+        log.info("came to service2");
 ////        Thread.sleep(sleep);
 //        log.info("going back from service2");
         return ResponseEntity.ok("From service2");
@@ -32,7 +30,8 @@ public class WebController {
     @SneakyThrows
     public ResponseEntity<String> errorEndpoint(){
 //        System.err.println("some error");
-        logger.error("Logging ERROR with Logback");
+        log.error("some random error errorEnd");
+//        logger.error("Logging ERROR with Logback");
         return ResponseEntity.ok("From service2 error");
     }
 }
