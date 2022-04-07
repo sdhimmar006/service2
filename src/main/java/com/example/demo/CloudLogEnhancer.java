d756791b1221c19b8a18a8a0c34d016389f399f7
RamyaG85 <ramya_gullapalli@yahoo.com>
2022-04-07 14:55:21 +0530

    Added Cloud Log Enhancer class

---
+++ src/main/java/com/example/demo/CloudLogEnhancer.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import com.google.cloud.logging.LoggingEnhancer;
+import com.google.cloud.logging.LogEntry;
+import com.google.cloud.logging.LoggingEnhancer;
+
+
+// Add / update additional fields to the log entry
+
+public class CloudLogEnhancer implements LoggingEnhancer {
+
+    @Override
+    public void enhanceLogEntry(LogEntry.Builder logEntry) {
+        // add additional labels
+        logEntry.addLabel("test-label-1", "test-value-1");
+    }
+
+}
